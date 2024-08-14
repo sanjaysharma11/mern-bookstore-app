ed046e8095113c38510219e6dccd5f6a3347d198
Your Name <12202080601123@adit.ac.in>
2024-08-15 00:04:21 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import path from "path";
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
@@ -30,6 +30,16 @@ try {
 // defining routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
+
+// deployment
+
+if(process.env.NODE_ENV === 'production'){
+    const dirPath = path.resolve();
+    app.use(express.static("./Frontend/dist"))
+    app.get("*", (req,res) =>{
+        res.sendFile(path.resolve(dirPath,"./Frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
