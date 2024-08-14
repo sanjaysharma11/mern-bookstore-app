4411ee7ab5c92a326beaa030dcb96f017fd21f27
Sanjay Kumar Sharma <125959746+sanjaysharma11@users.noreply.github.com>
2024-08-15 03:02:30 +0530

    Course.jsx

--- Backend/Frontend/src/components/Course.jsx
+++ Backend/Frontend/src/components/Course.jsx
@@ -7,7 +7,7 @@ function Course() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://mern-bookstore-app-1.onrender.com//book");
         console.log(res.data);
         setBook(res.data);
       } catch (error) {
